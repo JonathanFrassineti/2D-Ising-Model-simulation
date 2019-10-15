f1dba7ae98de8479910bf05c4aa719eeb8319fac
Jonathan Frassineti <jonathan.frassineti@studio.unibo.it>
2019-10-15 14:38:21 +0200

    Modified the Ising file with the beginning of the class structure.

--- Ising.py
+++ Ising.py
@@ -8,5 +8,10 @@ import numpy as np
 from numpy.random import rand
 import matplotlib.pyplot as plt
 
-class Ising:
-    
+class Ising():
+    "This class simulates the Ising model in 2 dimensions."
+    def __init__(self, state=10):
+        self.state = state
+        
+if __name__ == '__main__':
+    model = Ising()
