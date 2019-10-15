6f37030a50d5643b9a54c36064eceb5a447b25e9
Jonathan Frassineti <jonathan.frassineti@studio.unibo.it>
2019-10-15 14:05:31 +0200

    Added the first version of Ising file.

---
+++ Ising.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 15 14:01:39 2019
+
+@author: Jonathan Frassineti
+"""
+import numpy as np
+from numpy.random import rand
+import matplotlib.pyplot as plt
+
+class Ising:
+    
