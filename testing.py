5da85fe2d1d5f18e9261457229e842ffa3d6767b
Jonathan Frassineti <jonathan.frassineti@studio.unibo.it>
2019-10-17 22:21:59 +0200

    Testes the second method that generates a MonteCarlo move and so a different state of the lattice.

--- testing.py
+++ testing.py
@@ -7,13 +7,48 @@ Created on Tue Oct 15 14:09:18 2019
 
 from ising import Ising
 import numpy as np
+import copy
 
-N = 100
+"Length of the 2D square spin lattice."
+N = 15
+"Number of temperature intervals."
+nt = 50
+"Number of steps used to equilibrate the syste with MonteCarlo algorithm."
+eqsteps = 100
+"Temperature array T from 1.0 to 4.0 (unitless)."
+T = np.linspace(1.0,4.0,nt)
+"Creation of an Ising object."
+state = Ising()
 
 def test_initialstate():
-    state = Ising()
-    model =state.initialstate(N)
-    assert model.all() == 1 or model.all() == -1
+    "Initialazing the model with N*N spins of values 1 and -1."
+    model = state.initialstate(N) 
+    abs_model = np.abs(model)
+    "Test if all the spins have really the values 1 and -1."
+    assert abs_model.all() == 1 
+    
+def test_montmove():
+    "Do a cycle in order to range from lowest temperature to highest one."
+    for tt in range(nt):
+        "Calculation of beta = 1/kT, where k is put equal to 1 for semplicity."
+        it = 1.0/T[tt]
+        model = state.initialstate(N)
+        "Copy the original state in order to compare it with the modified one."
+        init = copy.deepcopy(model)
+        "Montecarlo moves from the original state to the modified one."
+        for i in range (eqsteps):
+            config = state.montmove(N,model,it)
+        diff = config - init
+        "Test if the modified state is really different from the original one."
+        assert np.any(diff) == True
+                                        
+       
+if __name__ == "main":
+    pass                
             
-if __name__ == '__main__':
-    pass
+            
+
+
+        
+         
+            
