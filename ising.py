f73cb0dca3bfb21a816f6ebaf73f99ca3c82470a
Jonathan Frassineti <jonathan.frassineti@studio.unibo.it>
2019-10-18 18:11:53 +0200

    Added the third method to the ising class that calculates the energy of a given configuration of spins in the lattice.

--- ising.py
+++ ising.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 
 class Ising():
     "This class simulates the Ising model in 2 dimensions."
+    
     def initialstate(self,N):
         "This method generates a random spin configuration for initial condition."
         state = np.random.choice([-1,1],size=(N,N))
@@ -37,6 +38,17 @@ class Ising():
                 "New state of the spin."    
                 config[x,y] = z          
         return config
+    
+    def energy(self,N,config):
+        "This method calculates the energy of a given configuration, given the exchange constant J = 1."
+        energy = 0
+        for i in range(len(config)):
+            for j in range(len(config)):
+                zz = config[i,j]
+                ss = config[(i+1)%N,j] + config[i,(j+1)%N] + config[(i-1)%N,j] + config[i,(j-1)%N]
+                "The change in energy is given by the product of the (x,y) spin and the 4 nearest neighbours spins."
+                energy += -ss*zz
+        return energy/4
 
 if __name__ == "main":
     pass  
