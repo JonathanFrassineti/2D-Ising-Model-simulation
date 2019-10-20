e627b775701e296c193f42d924f004f00729721f
Jonathan Frassineti <jonathan.frassineti@studio.unibo.it>
2019-10-20 20:49:27 +0200

    Update testing.py.

--- testing.py
+++ testing.py
@@ -9,54 +9,54 @@ from ising import Ising
 import numpy as np
 import copy
 
-"Length of the 2D square spin lattice."
+#Length of the 2D square spin lattice."
 N = 15
-"Number of temperature intervals."
+#Number of temperature intervals."
 nt = 50
-"Number of steps used to equilibrate the syste with MonteCarlo algorithm."
+#Number of steps used to equilibrate the syste with MonteCarlo algorithm."
 eqsteps = 100
-"Temperature array T from 1.0 to 4.0 (unitless)."
+#Temperature array T from 1.0 to 4.0 (unitless)."
 T = np.linspace(1.0,4.0,nt)
-"Creation of an Ising object."
+#Creation of an Ising object."
 state = Ising()
 
 def test_initialstate():
-    "Initialazing the model with N*N spins of values 1 and -1."
+    #Initialazing the model with N*N spins of values 1 and -1."
     model = state.initialstate(N) 
     abs_model = np.abs(model)
-    "Test if all the spins have really the values 1 and -1."
+    #Test if all the spins have really the values 1 and -1."
     assert abs_model.all() == 1 
     
 def test_montmove():
-    "Do a cycle in order to range from lowest temperature to highest one."
+    #Do a cycle in order to range from lowest temperature to highest one."
     for tt in range(nt):
-        "Calculation of beta = 1/kT, where k is put equal to 1 for semplicity."
+        #Calculation of beta = 1/kT, where k is put equal to 1 for semplicity."
         it = 1.0/T[tt]
         model = state.initialstate(N)
-        "Copy the original state in order to compare it with the modified one."
+        #Copy the original state in order to compare it with the modified one."
         init = copy.deepcopy(model)
-        "Montecarlo moves from the original state to the modified one."
+        #Montecarlo moves from the original state to the modified one."
         for i in range (eqsteps):
             config = state.montmove(N,model,it)
         diff = config - init
-        "Test if the modified state is really different from the original one."
+        #Test if the modified state is really different from the original one."
         assert np.any(diff) == True
 
 def test_energy():
-    "This method calculates the energy of a given configuration, given the exchange constant J = 1."
+    #This method calculates the energy of a given configuration, given the exchange constant J = 1."
     energy = 0
-    "Copy the initial energy in order to compare it with the modified one."
+    #Copy the initial energy in order to compare it with the modified one."
     init = copy.deepcopy(energy)
     model = state.initialstate(N)
-    "Test if the final energy is really different from the initial energy."
+    #Test if the final energy is really different from the initial energy."
     assert state.energy(N, model) != init
         
 def test_mag():
-    "This method calculates the magnetization of a given configuration."
+    #This method calculates the magnetization of a given configuration."
     model = state.initialstate(N)
-    "Test if the magnetization is really the sum of all the elements of the configuration."
+    #Test if the magnetization is really the sum of all the elements of the configuration."
     assert state.mag(model) == np.sum(model)
-    "Test if the magnetization is different from 0."
+    #Test if the magnetization is different from 0."
     assert state.mag(model) != 0
        
 if __name__ == "main":
