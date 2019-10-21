c998059e204048bef9263e6db7889cb97e9d9c5d
Jonathan Frassineti <jonathan.frassineti@studio.unibo.it>
2019-10-21 16:18:46 +0200

    Added the fifth method that simulates the Ising model for a given configuration at a specific T during the time, and a method that plots the results.

--- ising.py
+++ ising.py
@@ -10,50 +10,78 @@ import matplotlib.pyplot as plt
 
 
 class Ising():
-    #This class simulates the Ising model in 2 dimensions."
+    #This class simulates the Ising model in 2 dimensions.
     
     def initialstate(self,N):
-        #This method generates a random spin configuration for initial condition."
+        #This method generates a random spin configuration for initial condition.
         state = np.random.choice([-1,1],size=(N,N))
         return state
         
     
     def montmove(self,N,config,beta):
-        #This method creates a Monte Carlo move using the Metropolis algorithm."
+        #This method creates a Monte Carlo move using the Metropolis algorithm.
         for i in range(N):
             for j in range(N):
                 x = np.random.randint(0,N)
                 y = np.random.randint(0,N)
-                #State of the (x,y) spin in the lattice."
+                #State of the (x,y) spin in the lattice.
                 z = config[x,y]
-                #State of the nearest neighbours spins in the lattice (there are 4)."
+                #State of the nearest neighbours spins in the lattice (there are 4).
                 s = config[(x+1)%N,y] + config[x,(y+1)%N] + config[(x-1)%N,y] + config[x,(y-1)%N]
-                #Energy change if spin (x,y) is flipped, according to the surrounding spins."         
+                #Energy change if spin (x,y) is flipped, according to the surrounding spins."        
                 cost = 2*s*z
-                #If the energy change is negative, accept the move and flip the spin, otherwise accept the move with probability exp(-cost*beta), and flip the spin."
+                #If the energy change is negative, accept the move and flip the spin, otherwise accept the move with probability exp(-cost*beta), and flip the spin.
                 if cost < 0 :
                     z *= -1
                 elif rand() < np.exp(-cost*beta):
                     z *= -1  
-                #New state of the spin."    
+                #New state of the spin."  
                 config[x,y] = z          
         return config
     
     def energy(self,N,config):
-        #This method calculates the energy of a given configuration, given the exchange constant J = 1."
+        #This method calculates the energy of a given configuration, given the exchange constant J = 1.
         energy = 0
         for i in range(len(config)):
             for j in range(len(config)):
                 zz = config[i,j]
                 ss = config[(i+1)%N,j] + config[i,(j+1)%N] + config[(i-1)%N,j] + config[i,(j-1)%N]
-                #The change in energy is given by the product of the (x,y) spin and the 4 nearest neighbours spins."
+                #The change in energy is given by the product of the (x,y) spin and the 4 nearest neighbours spins.
                 energy += -ss*zz
         return energy/4
     
     def mag(self,config):
-        #This method calculates the magnetization of a given configuratiin of spins."
+        #This method calculates the magnetization of a given configuratiin of spins.
         magn = np.sum(config)
-        return magn  
-
+        return magn
+        
+    
+    def simulate(self,N):   
+        #This module simulates the Ising model lattice for a given temperature, under the critical temperature, where the systemis ordered (ferromagnetic state).
+        temp = 1 # Initialise the lattice with temperature equal to 1.
+        configuration = np.random.choice([-1,1],size=(N,N))
+        f = plt.figure(figsize=(15, 15), dpi=80);    
+        self.configurationPlot(f, configuration, 0, N, 1)
+        msrmnt = 1001
+        for i in range(msrmnt):
+            a = self.montmove(N,configuration, 1.0/temp)
+            if i == 1:       self.configurationPlot(f, configuration, i, N, 2)
+            if i == 4:       self.configurationPlot(f, configuration, i, N, 3)
+            if i == 32:      self.configurationPlot(f, configuration, i, N, 4)
+            if i == 100:     self.configurationPlot(f, configuration, i, N, 5)
+            if i == 1000:    self.configurationPlot(f, configuration, i, N, 6)
+        return a
+        f.savefig('configPlot.png')    
+        
+    def configurationPlot(self, f, configuration, i, N, n):
+        # This module plots the configuration once passed to it along with time, for a given T.
+        X, Y = np.meshgrid(range(N), range(N))
+        sp =  f.add_subplot(3, 3, n)  
+        plt.setp(sp.get_yticklabels(), visible=False)
+        plt.setp(sp.get_xticklabels(), visible=False)      
+        plt.pcolormesh(X, Y, configuration, cmap=plt.cm.RdBu)
+        plt.title('Time=%d'%i); plt.axis('tight')    
+    plt.show()
+                    
 if __name__ == "main":
     pass  
