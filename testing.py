d0b4641b16980051bb15f20b8311e62fd5ae1002
Jonathan Frassineti <jonathan.frassineti@studio.unibo.it>
2019-10-25 03:20:09 +0200

    Update tetsing.py.

--- testing.py
+++ testing.py
@@ -5,67 +5,49 @@ Created on Tue Oct 15 14:09:18 2019
 @author: Jonathan Frassineti
 """
 
-from ising import Ising
+import ising
+import configuration
 import numpy as np
-import copy
+import hypothesis
+from hypothesis import strategies as st
+from hypothesis import settings
+from hypothesis import given
 
-#Length of the 2D square spin lattice."
-N = 15
-#Number of temperature intervals."
-nt = 50
-#Number of steps used to equilibrate the syste with MonteCarlo algorithm."
-eqsteps = 100
-#Temperature array T from 1.0 to 4.0 (unitless)."
-T = np.linspace(1.0,4.0,nt)
-#Creation of an Ising object."
-state = Ising()
-
-def test_initialstate():
-    #Initialazing the model with N*N spins of values 1 and -1."
-    model = state.initialstate(N) 
+@given(value1=st.integers(1,configuration.N), value2 = st.integers(1,configuration.M))
+@settings(max_examples = 1)
+def test_initialstate(value1,value2):
+    #Initialazing the model with N*M spins of values 1 and -1."
+    model = ising.initialstate(value1,value2) 
     abs_model = np.abs(model)
     #Test if all the spins have really the values 1 and -1."
     assert abs_model.all() == 1 
-    
-def test_montmove():
+
+@given(value1=st.integers(1,configuration.N), value2 = st.integers(1,configuration.M), members = st.integers(50,configuration.numberTemp), values = st.integers(100,configuration.eqSteps))
+@settings(max_examples = 1)   
+def test_montmove(value1, value2, members, values):
     #Do a cycle in order to range from lowest temperature to highest one."
-    for tt in range(nt):
+    for tempInterval in range(members):
         #Calculation of beta = 1/kT, where k is put equal to 1 for semplicity."
-        it = 1.0/T[tt]
-        model = state.initialstate(N)
+        model = ising.initialstate(value1,value2)
         #Copy the original state in order to compare it with the modified one."
-        init = copy.deepcopy(model)
+        init = model.copy()
         #Montecarlo moves from the original state to the modified one."
-        for i in range (eqsteps):
-            config = state.montmove(N,model,it)
-        diff = config - init
-        #Test if the modified state is really different from the original one."
-        assert np.any(diff) == True
+        for i in range (values):
+            config = ising.montmove(model,configuration.inverseTemp[tempInterval])
+        #Test if the final state is different from the initial one.
+        assert ising.calculateEnergy(config) <= ising.calculateEnergy(init)
+        assert np.abs(ising.calculateMagn(config)) >= np.abs(ising.calculateMagn(config))
 
-def test_energy():
-    #This method calculates the energy of a given configuration, given the exchange constant J = 1."
-    energy = 0
-    #Copy the initial energy in order to compare it with the modified one."
-    init = copy.deepcopy(energy)
-    model = state.initialstate(N)
-    #Test if the final energy is really different from the initial energy."
-    assert state.energy(N, model) != init
-        
-def test_mag():
-    #This method calculates the magnetization of a given configuration."
-    model = state.initialstate(N)
-    #Test if the magnetization is really the sum of all the elements of the configuration."
-    assert state.mag(model) == np.sum(model)
-    #Test if the magnetization is different from 0."
-    assert state.mag(model) != 0
-
-def test_simulate():
+@given(value1=st.integers(1,configuration.N), value2 = st.integers(1,configuration.M))
+@settings(max_examples = 1)
+def test_simulate(value1, value2):
     #This method simulates the Ising model of a given configuration for a specific T."
-    x = state.initialstate(N)
-    y = state.simulate(N)
-    diff = x - y 
-    assert np.any(diff) == True  #Test if the final configuration is really different form the inital one"
-    assert np.sum(y) != np.sum(x) #Test if the final magnetization is different than the initial one.
+    initState = ising.initialstate(value1,value2)
+    finalState = ising.simulate(initState)
+    assert ising.calculateEnergy(finalState[len(finalState)-1]) <= ising.calculateEnergy(finalState[0])
+    assert np.abs(ising.calculateMagn(finalState[len(finalState)-1])) >= np.abs(ising.calculateMagn(finalState[0]))
+         
+        
 
 
 
