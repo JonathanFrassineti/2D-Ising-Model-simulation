4dd4f54c24e428e4f53e5f2d3ce3643b4148c351
Jonathan Frassineti <jonathan.frassineti@studio.unibo.it>
2019-11-19 11:33:01 +0100

    Update simulation.py.

--- simulation.py
+++ simulation.py
@@ -5,29 +5,51 @@ Created on Mon Oct 21 16:25:29 2019
 @author: Jonathan Frassineti
 """
 import ising
-import configuration
 import numpy as np
+import sys
+from sys import argv
+import configparser
 
 #main part of the code
-for tempInterval in range(configuration.numberTemp):
-    config = ising.initialstate(configuration.N,configuration.M)
+config = configparser.ConfigParser()
+config.read(sys.argv[1])
+
+N = config.get('settings', 'N')
+M = config.get('settings', 'M')
+numberTemp = config.get('settings', 'numberTemp')
+eqSteps = config.get('settings', 'eqSteps')
+
+N = int(N)
+M = int(M)
+numberTemp = int(numberTemp)
+eqSteps = int(eqSteps)
+
+T = np.linspace(1.50,3.50,numberTemp) 
+Energy = np.zeros(numberTemp)
+Magn = np.zeros(numberTemp)
+inverseTemp = 1.0/T 
+n1 = 1.0/(eqSteps*N*M) 
+
+for tempInterval in range(numberTemp):
+    
+    config = ising.initialstate(N,M)
     Energy1 = Magn1 = 0
     
-    for i in range(configuration.eqSteps):
-        ising.montmove(config,configuration.inverseTemp[tempInterval])
+    for i in range(eqSteps):
+        ising.montmove(config,inverseTemp[tempInterval])
 
-    for i in range(configuration.eqSteps):
-        ising.montmove(config,configuration.inverseTemp[tempInterval])           
+    for i in range(eqSteps):
+        ising.montmove(config,inverseTemp[tempInterval])           
         Energy = ising.calculateEnergy(config)     # calculate the energy
         Magn = ising.calculateMagn(config)        # calculate the magnetisation
         Energy1 += Energy
         Magn1 +=  Magn
 
-    configuration.Energy[tempInterval] = configuration.n1*Energy1
-    configuration.Magn[tempInterval] = configuration.n1*Magn1
+    Energy[tempInterval] = n1*Energy1
+    Magn[tempInterval] = n1*Magn1
     
 np.save('./data/ene',configuration.Energy)
 np.save('./data/mag',configuration.Magn)
 
-totalStates = ising.simulate(ising.initialstate(configuration.N,configuration.M))
+totalStates = ising.simulate(ising.initialstate(N,M))
 np.save('./data/time',np.asarray(totalStates))
