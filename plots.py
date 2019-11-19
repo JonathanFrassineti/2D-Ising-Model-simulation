64b864ccd923157594222ce192ed1441c6cb2fda
Jonathan Frassineti <jonathan.frassineti@studio.unibo.it>
2019-11-19 13:32:44 +0100

    Update plots.py.

--- plots.py
+++ plots.py
@@ -4,10 +4,24 @@ Created on Thu Oct 24 12:04:25 2019
 
 @author: Jonathan Frassineti
 """
-
-import configuration
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+from sys import argv
+import configparser
+
+config = configparser.ConfigParser()
+config.read(sys.argv[1])
+
+N = config.get('settings', 'N')
+M = config.get('settings', 'M')
+numberTemp = config.get('settings', 'numberTemp')
+
+N = int(N)
+M = int(M)
+numberTemp = int(numberTemp)
+
+T = np.linspace(1.50,3.50,numberTemp)
 
 def configurationPlot():
     """This module plots the configuration once 
@@ -20,10 +34,10 @@ def configurationPlot():
         i: time interval.
         N: length of the square lattice (N*N).
         n: number of subplot."""
-    config = np.load('./data/time.npy')
+    config = np.load(sys.argv[2])
     i = [0,1,4,32,100,1000]
     f = plt.figure(figsize=(15, 15), dpi=80)
-    X, Y = np.meshgrid(range(configuration.N), range(configuration.M))
+    X, Y = np.meshgrid(range(N), range(M))
     for n in range(len(i)):
         sp =  f.add_subplot(3, 3, n+1)  
         plt.setp(sp.get_yticklabels(), visible=False)
@@ -36,9 +50,8 @@ def configurationPlot():
 def graphPlot():
     """ This method plots the magnetization and the energy of the lattice.
     """
-    T = configuration.T.copy()
-    energy = np.load('./data/ene.npy')  
-    magnetization = np.load('./data/mag.npy')    
+    energy = np.load(sys.argv[3])  
+    magnetization = np.load(sys.argv[4])    
     f = plt.figure(figsize=(18, 18)) # plot the calculated values    
     sp = f.add_subplot(2, 2, 1 )
     plt.scatter(T, energy, s=50, marker='o', color='IndianRed')
